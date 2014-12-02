c78d00c5fa89894df888aae6234a18a09c4be261
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2014-12-02 09:41:46 -0500

    Added licensing

--- pyplink/__init__.py
+++ pyplink/__init__.py
@@ -1,10 +1,17 @@
 """A module to read Plink's binary files."""
 
+# This file is part of pyplink.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
 
 __author__ = "Louis-Philippe Lemieux Perreault"
-__copyright__ = "Copyright 2014, Beaulieu-Saucier Pharmacogenomics Centre"
+__copyright__ = "Copyright 2014 Louis-Philippe Lemieux Perreault"
 __credits__ = ["Louis-Philippe Lemieux Perreault"]
-__license__ = "GPL"
+__license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
 __version__ = "0.2"
 __maintainer__ = "Louis-Philippe Lemieux Perreault"
 __email__ = "louis-philippe.lemieux.perreault@statgen.org"
--- pyplink/pyplink.py
+++ pyplink/pyplink.py
@@ -1,6 +1,20 @@
 """Module that reads binary Plink files."""
 
+# This file is part of pyplink.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+__author__ = "Louis-Philippe Lemieux Perreault"
+__copyright__ = "Copyright 2014 Louis-Philippe Lemieux Perreault"
+__license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
+
+
 __all__ = ["PyPlink"]
+
 
 import os
 
