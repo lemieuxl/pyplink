1348eb48858e2025905025b2b6db170150ed91e8
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-06-23 13:33:24 -0400

    Changed for the MIT license

--- setup.py
+++ setup.py
@@ -51,7 +51,9 @@ def setup_package():
         classifiers=["Operating System :: Linux",
                      "Programming Language :: Python",
                      "Programming Language :: Python :: 2.7",
-                     "Programming Language :: Python :: 3.4"],
+                     "Programming Language :: Python :: 3.3",
+                     "Programming Language :: Python :: 3.4",
+                     "License :: OSI Approved :: MIT License"],
     )
 
     return
