626f95046763c72252903b349a0e87d69f95f901
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-06-23 13:27:05 -0400

    Version is now set in the setup.py script

--- setup.py
+++ setup.py
@@ -9,19 +9,53 @@
 import os
 from setuptools import setup
 
-from pyplink import __version__
+
+MAJOR = 0
+MINOR = 3
+VERSION = "{}.{}".format(MAJOR, MINOR)
 
 
-setup(name="pyplink",
-      version=__version__,
-      description="Python module to read binary Plink files.",
-      author="Louis-Philippe Lemieux Perreault",
-      author_email="louis-philippe.lemieux.perreault@statgen.org",
-      url="http://www.statgen.org",
-      license="GPL",
-      packages=["pyplink"],
-      install_requires=["numpy >= 1.8.2", "pandas >= 0.14.1"],
-      classifiers=['Operating System :: Linux',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.4'])
+def write_version_file(fn=None):
+    if fn is None:
+        fn = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            os.path.join("pyplink", "version.py"),
+        )
+
+    content = (
+        "\n# THIS FILE WAS GENERATED AUTOMATICALLY BY PYPLINK SETUP.PY\n"
+        'pyplink_version = "{version}"\n'
+    )
+
+    a = open(fn, "w")
+    try:
+        a.write(content.format(version=VERSION))
+    finally:
+        a.close()
+
+
+def setup_package():
+    # Saving the version into a file
+    write_version_file()
+
+    setup(
+        name="pyplink",
+        version=VERSION,
+        description="Python module to read binary Plink files.",
+        author="Louis-Philippe Lemieux Perreault",
+        author_email="louis-philippe.lemieux.perreault@statgen.org",
+        url="http://www.statgen.org",
+        license="GPL",
+        packages=["pyplink"],
+        install_requires=["numpy >= 1.8.2", "pandas >= 0.14.1"],
+        classifiers=["Operating System :: Linux",
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3.4"],
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    setup_package()
