de03806de3031efa6b7d969173ec77a1bae10d93
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2024-06-18 09:17:08 -0400

    Updated docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '1.3.5',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.3.8b1',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
